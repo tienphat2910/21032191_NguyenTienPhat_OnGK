3571a827ad14d53acbdf6bbe49169b6da025bece
Nguyen Tien Phat <imissu3403@gmail.com>
2024-10-19 22:39:45 +0700

    sua screen01

--- screens/Screen01.js
+++ screens/Screen01.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 
 const Screen01 = ({ navigation }) => {
     const users = [
-        { email: 'user1@example.com', password: 'password1' },
-        { email: 'user2@example.com', password: 'password2' },
-        { email: 'user3@example.com', password: 'password3' },
-        { email: 'user4@example.com', password: 'password4' },
-        { email: 'user5@example.com', password: 'password5' },
+        { email: 'user1@example.com', password: '123456' },
+        { email: 'user2@example.com', password: '123456' },
+        { email: 'user3@example.com', password: '123456' },
+        { email: 'user4@example.com', password: '123456' },
+        { email: 'user5@example.com', password: '123456' },
     ];
 
     const [email, setEmail] = useState('');
